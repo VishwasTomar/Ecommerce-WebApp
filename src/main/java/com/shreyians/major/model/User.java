713b93778932232b66c192f4cbb48772ceb457f6
Vishwas Tomar <vishwas7201@gmail.com>
2025-01-24 13:37:18 -0500

    i have added displaying page on shop and also created models for authentication , authentication-User detail.

---
+++ src/main/java/com/shreyians/major/model/User.java
@@ -0,0 +1,50 @@
+package com.shreyians.major.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private  String firstName;
+    private String lastNamee;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    @Email(message = "{errors.invalid_enail}")
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_role",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "id")},
+              inverseJoinColumns = {@JoinColumn (name = "Role_ID", referencedColumnName = "ID")}
+    )
+    private List<Role> roles;
+
+    public User(User user) {
+        this.firstName = user.getFirstName();
+        this.lastNamee = user.lastNamee;
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = user.roles;
+    }
+
+    public User(){
+
+    }
+}
